908dfeb340c998ae8dba58850a7549799739adf6
Himeth Dahanayake <himethdaha@gmail.com>
2021-03-13 10:56:04 -0500

    End of Section 2

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //IEumerable lets me iterate over a collection of a specified type
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return  users;
+        }
+
+        //controller/api/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+            
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
+using System;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //    :base(options)
+        //{ 
+        //
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public virtual Microsoft.EntityFrameworkCore.DbSet<AppUser> Users { get; set; }
+      
+
+
+        
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          
+        //{
+        //    optionsBuilder
+        //        .UseMySql("server = localhost; port = 3306; database = Rocky; user = blog; password = blog")
+        //        .UseLoggerFactory(LoggerFactory.Create(b => b
+        //               .AddConsole()
+        //               .AddFilter(level => level >= LogLevel.Information)))
+        //        .EnableSensitiveDataLogging()
+        //        .EnableDetailedErrors();
+        //}
+    }
+}
